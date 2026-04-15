ecd9008a8e599b0b6dba46cf63c5b35bdc53b4b5
sagar50sagar <sagar81054@gmail.com>
2026-04-15 12:50:04 +0530

    Update project files and AI/ML integration documentation

--- scratch/check_schema.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('campuscode.db');
-db.all("PRAGMA table_info(contests);", [], (err, rows) => {
-    if (err) console.error(err);
-    else console.log(JSON.stringify(rows, null, 2));
-    db.close();
-});
--- scratch/list_tables.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('database.sqlite');
-db.all("SELECT name FROM sqlite_master WHERE type='table';", [], (err, rows) => {
-    if (err) console.error(err);
-    else console.log(JSON.stringify(rows, null, 2));
-    db.close();
-});
